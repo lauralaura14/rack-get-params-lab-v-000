a1876a0d52cb48e56e272e295491f5abd8fdafd7
Laura <laurao148@gmail.com>
2018-04-16 21:46:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/cart/)
-      search_ = req.params["item"]
+      search_ter = req.params["item"]
         if @@items.include?(search_term)
           @@cart << search_term
           resp.write "added #{search_term}\n"
